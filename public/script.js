e781929eb7ff5d90d04eff4685b965761f2461d2
Khadijah Clark <khadijah.clark.2@gmail.com>
2024-12-13 21:12:49 -0600

    Got the progress bar to work

--- public/script.js
+++ public/script.js
@@ -11,4 +11,23 @@
 //         loader.classList.add("hidden");
 //     };
 // });
+window.onload = function () {
+    const container = document.getElementById("inner-progress-container");
+    
+    const numBlocks = 13;
 
+    let currentBlock = 0;
+
+    const interval = setInterval(() => {
+        if (currentBlock < numBlocks) {
+            const block = document.createElement("div");
+            block.setAttribute("id", "progress-block");
+            block.classList.add("bg-cyan-950", "w-5", "h-5", "ml-1");
+            container.appendChild(block);
+            currentBlock++;
+        } else {
+            clearInterval(interval); 
+        }
+    }, 200); 
+
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,7 @@ module.exports = {
   content: ['./public/index.html',
   './src/**/*.{html,js}',],
   theme: {
-    extend: {
-      keyframes: {
-        fadeOut: {
-          '0%': { opacity: '1'},
-          '100%': { opacity: '0'}
-        }
-      },
-      animation: {
-        fadeOut: ' fadeOut 1.5s ease-out'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
