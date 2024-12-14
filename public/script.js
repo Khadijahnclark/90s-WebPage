ad3a96fa3c7bcd3ed5ff9d9320e8aa5f26ebcd44
Khadijah Clark <khadijah.clark.2@gmail.com>
2024-12-13 21:55:03 -0600

    Getting the progress asnd loading screen functionality working

--- public/script.js
+++ public/script.js
@@ -1,17 +1,6 @@
 //loading Screen
-// const loader = document.
-
-// window.addEventListener("load", ()=>{
-
-//     setTimeout(() =>{
-//         deactivate()
-//     }, 3000)
-
-//     const deactivate = () => {
-//         loader.classList.add("hidden");
-//     };
-// });
 window.onload = function () {
+    const loader = document.getElementById("loader");
     const container = document.getElementById("inner-progress-container");
     
     const numBlocks = 13;
@@ -24,10 +13,14 @@ window.onload = function () {
             block.setAttribute("id", "progress-block");
             block.classList.add("bg-cyan-950", "w-5", "h-5", "ml-1");
             container.appendChild(block);
-            currentBlock++;
+            currentBlock += 1;
         } else {
+            deactivate();
             clearInterval(interval); 
         }
-    }, 200); 
+    }, 200);
 
+    const deactivate = () => {
+        loader.classList.add("hidden");
+    };
 };
